a60c7825d5ee9134294b9edcb53aba405d6c1a33
German Lashevich <german.lashevich@gmail.com>
2021-09-08 01:17:03 +0200

    Add package installer script

---
+++ run_once_install-packages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+sudo pacman -S --needed git base-devel
+
+if ! command -v yay &> /dev/null
+then
+    readonly YAY_DIR=/tmp/yay
+    rm -rf $YAY_DIR
+    git clone https://aur.archlinux.org/yay.git $YAY_DIR
+    cd $YAY_DIR
+    makepkg -si
+fi
+
+yay -S --needed --answerclean N --answerdiff N \
+    antibody-bin \
+    bat \
+    exa \
+    fd \
+    fzf \
+    neovim \
+    ripgrep \
+    tmux \
+    z \
+    zsh \
+    zsh-completions
