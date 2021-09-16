2d38d7d8293e266a7eab05272c10570d6b21ddab
German Lashevich <german.lashevich@gmail.com>
2021-09-16 22:59:23 +0200

    Restart emacs daemon after update

--- run_emacs-update.sh
+++ run_emacs-update.sh
@@ -7,3 +7,5 @@ readonly EMACS_HOME="${HOME}/.emacs.d"
 git -C "$EMACS_HOME" pull
 
 emacs --batch -l "${EMACS_HOME}/init.el" --eval="(configuration-layer/update-packages t)"
+
+systemctl restart --user emacs
