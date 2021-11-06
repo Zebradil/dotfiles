a19ee07d4d6656693cabb097637490d88ca6c6e0
German Lashevich <german.lashevich@gmail.com>
2021-11-06 13:56:19 +0100

    Update run_update-antibody.sh

--- run_update-antibody.sh
+++ run_update-antibody.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 
 printf ' :Updating Antibody: ' | xargs -d: -n1 printf '\033[1;30;42m %-40s \033[0m\n'
 
-antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
 antibody update
