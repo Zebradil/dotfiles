c0686abab76fa9b37155de7c6b6c59221d0f102a
German Lashevich <german.lashevich@gmail.com>
2022-03-22 11:00:48 +0100

    Revert "Update private_dot_config/lvim/config.lua"
    This reverts commit ec3f6200f5079b681fe5b6a68d82ec270f2a406b.

--- private_dot_config/lvim/config.lua
+++ private_dot_config/lvim/config.lua
@@ -58,32 +58,26 @@ lvim.builtin.dashboard.active = true
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 1
+lvim.builtin.nvimtree.show_icons.git = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
   "c",
-  "css",
-  "hcl",
-  "java",
   "javascript",
   "json",
   "lua",
   "python",
-  "rust",
-  "tsx",
   "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
   "yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
-
-local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
-parser_configs.hcl = {
-  filetype = "hcl", "terraform",
-}
 
 -- generic LSP settings
 
