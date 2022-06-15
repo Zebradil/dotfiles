016b2022fdbe166d5386befbfa64539a8fa929b4
German Lashevich <german.lashevich@gmail.com>
2022-06-15 23:10:49 +0200

    Update run_update-rust.sh

--- run_update-rust.sh
+++ run_update-rust.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+set -euo pipefail
+
+if ! command -v rustup &>/dev/null; then
+    printf " /Skip update: rustup is not installed/ " | xargs -d/ -n1 printf '\033[1;30;43m %-40s \033[0m\n'
+    exit 0
+fi
+
 printf ' :Updating Rust: ' | xargs -d: -n1 printf '\033[1;30;42m %-40s \033[0m\n'
 
 rustup update
