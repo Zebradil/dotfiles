7e04caad84e768ec72658d6d526753a86eaa8d65
German Lashevich <german.lashevich@gmail.com>
2023-01-29 23:12:00 +0100

    Update src/.zsh/zshrc/tools/gnome-keyring.sh

--- src/dot_zsh/zshrc/tools/gnome-keyring.sh
+++ src/dot_zsh/zshrc/tools/gnome-keyring.sh
@@ -15,7 +15,7 @@ fi
 if lib::check_commands systemd-ask-password; then
   log::debug "Configuring gnome-keyring functions"
 
-  my:gnome-keyring:unlock() {
+  my:gnome-keyring:is-locked() {
     keyring_locked="$(
       qdbus \
       org.gnome.keyring \
@@ -23,7 +23,19 @@ if lib::check_commands systemd-ask-password; then
       org.freedesktop.Secret.Collection.Locked
     )"
     if [[ $keyring_locked == "true" ]]; then
-        systemd-ask-password | unlock.py
+      return 0
+    else
+      return 1
+    fi
+  }
+
+  my:gnome-keyring:unlock() {
+    if my:gnome-keyring:is-locked; then
+        if systemd-ask-password | unlock.py; then
+          log::info "Unlocked gnome-keyring"
+        else
+          log::error "Failed to unlock gnome-keyring"
+        fi
     fi
   }
 fi
