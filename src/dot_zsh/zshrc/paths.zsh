4a894b45f00ed2608d83ff09805aa9dbe7397e7a
German Lashevich <german.lashevich@gmail.com>
2023-01-29 23:29:42 +0100

    Add src/bin/unlock.py Update src/.zsh/.zshrc Update src/.zsh/zshrc/paths.zsh

--- src/dot_zsh/zshrc/paths.zsh
+++ src/dot_zsh/zshrc/paths.zsh
@@ -2,12 +2,14 @@
 # | PATHs                   |
 # +-------------------------+
 
-export PATH="$HOME/.cargo/bin:${PATH}"
-export PATH="$HOME/.config/composer/vendor/bin:${PATH}"
-export PATH="$HOME/.emacs_doom/bin:${PATH}"
-export PATH="$HOME/.krew/bin:${PATH}"
-export PATH="$HOME/.local/bin:${PATH}"
-export PATH="$HOME/go/bin:${PATH}"
-export PATH="$HOME/bin:${PATH}"
+typeset -TUx PATH path
+
+path=("$HOME/.cargo/bin" "${path[@]}")
+path=("$HOME/.config/composer/vendor/bin" "${path[@]}")
+path=("$HOME/.emacs_doom/bin" "${path[@]}")
+path=("$HOME/.krew/bin" "${path[@]}")
+path=("$HOME/.local/bin" "${path[@]}")
+path=("$HOME/go/bin" "${path[@]}")
+path=("$HOME/bin" "${path[@]}")
 
 log::debug "PATH: $PATH"
