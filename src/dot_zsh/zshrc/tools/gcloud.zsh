e3e47162974a98cc62db749c48d26eed1f040d53
German Lashevich <german.lashevich@gmail.com>
2023-02-04 01:47:02 +0100

    Update src/.zsh/zshrc/tools/gcloud.zsh

--- src/dot_zsh/zshrc/tools/gcloud.zsh
+++ src/dot_zsh/zshrc/tools/gcloud.zsh
@@ -5,5 +5,8 @@
 if lib::check_commands gcloud; then
   log::debug "Configuring gcloud"
   export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+  if [[ -r /opt/google-cloud-sdk/completion.zsh.inc ]]; then
+    source /opt/google-cloud-sdk/completion.zsh.inc
+  fi
 fi
 
