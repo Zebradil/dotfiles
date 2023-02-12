96d16b3c052d5f53c9d092dfe04d7f21c3ed9b08
German Lashevich <german.lashevich@gmail.com>
2023-02-12 02:35:06 +0100

    Add src/lua/custom/chadrc.lua Add src/lua/custom/highlights.lua Add src/lua/custom/init.lua Add src/lua/custom/mappings.lua Add src/lua/custom/plugins/init.lua Add src/lua/custom/plugins/lspconfig.lua Add src/lua/custom/plugins/null-ls.lua Add src/lua/custom/plugins/overrides.lua

---
+++ src/lua/custom/plugins/init.lua
@@ -0,0 +1,73 @@
+local overrides = require "custom.plugins.overrides"
+
+---@type {[PluginName]: NvPluginConfig|false}
+local plugins = {
+
+  -- Dashboard
+  ["goolord/alpha-nvim"] = { disable = false },
+
+  -- Override plugin definition options
+  ["neovim/nvim-lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+  },
+
+  ["nvim-telescope/telescope.nvim"] = {
+    override_options = overrides.telescope,
+  },
+
+  -- overrde plugin configs
+  ["nvim-treesitter/nvim-treesitter"] = {
+    override_options = overrides.treesitter,
+  },
+
+  ["williamboman/mason.nvim"] = {
+    override_options = overrides.mason,
+  },
+
+  ["nvim-tree/nvim-tree.lua"] = {
+    override_options = overrides.nvimtree,
+  },
+
+  ["hrsh7th/nvim-cmp"] = {
+    override_options = overrides.cmp,
+  },
+
+  -- Install a plugin
+  ["max397574/better-escape.nvim"] = {
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+
+  -- code formatting, linting etc
+  ["jose-elias-alvarez/null-ls.nvim"] = {
+    after = "nvim-lspconfig",
+    config = function()
+      require "custom.plugins.null-ls"
+    end,
+  },
+
+  ["zbirenbaum/copilot.lua"] = {
+    config = function()
+      require("copilot").setup {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      }
+    end,
+  },
+  ["zbirenbaum/copilot-cmp"] = {
+    after = { "copilot.lua", "nvim-cmp" },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+
+  -- remove plugin
+  -- ["hrsh7th/cmp-path"] = false,
+}
+
+return plugins
---
+++ src/lua/custom/plugins/overrides.lua
@@ -0,0 +1,121 @@
+local M = {}
+
+M.telescope = {
+  defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "--follow",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--hidden",
+      "--no-ignore-vcs",
+    },
+  },
+}
+
+M.treesitter = {
+  ensure_installed = {
+    "bash",
+    "css",
+    "html",
+    "javascript",
+    "lua",
+    "vim",
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+
+    -- Shell
+    "shfmt",
+    "shellcheck",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+    ignore = false,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+M.cmp = function()
+  local cmp = require "cmp"
+  return {
+    experimental = {
+      ghost_text = true,
+    },
+    preselect = cmp.PreselectMode.None,
+    sources = {
+      { name = "copilot", group_index = 2 },
+      { name = "nvim_lsp", group_index = 2 },
+      { name = "nvim_lua", group_index = 2 },
+      { name = "luasnip", group_index = 2 },
+      { name = "buffer", group_index = 2 },
+      { name = "path", group_index = 2 },
+    },
+    mappings = {
+      ["<CR>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = false,
+      },
+    },
+    formatting = {
+      format = function(entry, vim_item)
+        local icons = require("nvchad_ui.icons").lspkind
+        if entry.source.name == "copilot" then
+          vim_item.kind = "  Copilot"
+          vim_item.kind_hl_group = "CmpItemKindCopilot"
+          return vim_item
+        end
+        vim_item.kind = string.format("%s %s", icons[vim_item.kind], vim_item.kind)
+        return vim_item
+      end,
+    },
+    sorting = {
+      priority_weight = 2,
+      comparators = {
+
+        require("copilot_cmp.comparators").prioritize,
+        require("copilot_cmp.comparators").score,
+        cmp.config.compare.exact,
+
+        -- Below is the default comparitor list and order for nvim-cmp
+        -- cmp.config.compare.offset,
+        cmp.config.compare.score,
+        cmp.config.compare.recently_used,
+        cmp.config.compare.locality,
+        cmp.config.compare.kind,
+        cmp.config.compare.sort_text,
+        cmp.config.compare.length,
+        cmp.config.compare.order,
+      },
+    },
+  }
+end
+
+return M
