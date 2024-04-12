c0af59de3341ccb23fe78ce634c31deb660037d1
German Lashevich <german.lashevich@gmail.com>
2024-04-12 11:55:06 +0200

    Update src/.editorconfig Update src/.zsh/zshrc/tools/tailscale.zsh

--- src/dot_zsh/zshrc/tools/tailscale.zsh
+++ src/dot_zsh/zshrc/tools/tailscale.zsh
@@ -3,10 +3,17 @@
 # +--------------------------+
 
 if lib::check_commands tailscale; then
+  log::debug "Configuring tailscale aliases"
   my:tailscale:switch() {
     local tailnet="$1"
+    log::info "Switching to tailscale networks requires sudo"
+    sudo --validate
     if [[ -z $tailnet ]]; then
-      tailnet=$(sudo tailscale switch --list | head -n +2 | fzf | awk '{print $1}')
+      tailnet=$(sudo tailscale switch --list | tail -n +2 | fzf | awk '{print $1}')
+      if [[ -z $tailnet ]]; then
+        log::error "No network selected"
+        return 1
+      fi
     fi
     sudo tailscale switch "$tailnet"
   }
