9468d6c7571f21c048f659299be5398b0585ef8d
German Lashevich <german.lashevich@gmail.com>
2024-05-13 12:36:43 +0200

    remove unused nvim profiles

--- src/private_dot_config/nvim-profiles/chad/nvim/lua/custom/init.lua
+++
@@ -1,9 +0,0 @@
-local autocmd = vim.api.nvim_create_autocmd
-
-vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
-
--- Auto resize panes when resizing nvim window
-autocmd("VimResized", {
-  pattern = "*",
-  command = "tabdo wincmd =",
-})
