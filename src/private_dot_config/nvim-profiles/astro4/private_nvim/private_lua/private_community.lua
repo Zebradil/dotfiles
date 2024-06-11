6cc3b4534beb1b2cf6830268c8e1a1e2c3494068
German Lashevich <german.lashevich@gmail.com>
2024-06-11 15:23:32 +0200

    Update src/.config/nvim-profiles/astro4/nvim/lua/community.lua

--- src/private_dot_config/nvim-profiles/astro4/private_nvim/private_lua/private_community.lua
+++ src/private_dot_config/nvim-profiles/astro4/private_nvim/private_lua/private_community.lua
@@ -15,6 +15,7 @@ return {
   { import = "astrocommunity.pack.just" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.nix" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.terraform" },
