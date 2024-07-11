ddea171a331d7c88f32d7bbfe914401acf09741a
German Lashevich <german.lashevich@gmail.com>
2024-07-11 20:33:37 +0200

    Remove emacs

--- src/dot_zsh/zshrc/paths.zsh
+++ src/dot_zsh/zshrc/paths.zsh
@@ -6,7 +6,6 @@ typeset -TUx PATH path
 
 path=("$HOME/.cargo/bin" "${path[@]}")
 path=("$HOME/.config/composer/vendor/bin" "${path[@]}")
-path=("$HOME/.emacs_doom/bin" "${path[@]}")
 path=("$HOME/.krew/bin" "${path[@]}")
 path=("$HOME/.local/bin" "${path[@]}")
 path=("$HOME/go/bin" "${path[@]}")
