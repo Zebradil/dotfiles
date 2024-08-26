36d6509b4638bb1284dab715a24e842ebc4c6f00
German Lashevich <german.lashevich@gmail.com>
2024-08-27 00:56:53 +0200

    Update src/.zsh/zshrc/paths.zsh

--- src/dot_zsh/zshrc/paths.zsh
+++ src/dot_zsh/zshrc/paths.zsh
@@ -9,6 +9,7 @@ path=("$HOME/.config/composer/vendor/bin" "${path[@]}")
 path=("$HOME/.krew/bin" "${path[@]}")
 path=("$HOME/.local/bin" "${path[@]}")
 path=("$HOME/go/bin" "${path[@]}")
+path=("$HOME/.nix-profile/bin" "${path[@]}")
 path=("$HOME/bin" "${path[@]}")
 
 log::debug "PATH: $PATH"
